4d0871b44213035fcbf3b968e7ba444ed5cf40b3
AJIT KUMAR <ajitm.ug21.cs@nitp.ac.in>
2024-07-29 22:34:07 +0530

    seconf commit

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ import userRoute from "./routes/userRoute.js";
 import messageRoute from "./routes/messageRoute.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+const path=require('path')
 import { app,server } from "./socket/socket.js";
 dotenv.config({});
 
@@ -26,8 +27,19 @@ app.use(cors(corsOption));
 // routes
 app.use("/api/v1/user",userRoute); 
 app.use("/api/v1/message",messageRoute);
- 
-
+ // --------------deployment code-------
+ const __dirname1=path.resolve();
+ if(process.env.NODE_ENV==='production'){
+    app.use(express.static(path.join(__dirname1,"/frontend/build")));
+   app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(__dirname1,"frontend","build","index.html"))
+   })
+}
+ else{
+app.get("/",(req,res)=>{
+    res.send("API is running Successfully");
+})
+ }
 server.listen(PORT, ()=>{
     connectDB();
     console.log(`Server listen at prot ${PORT}`);
